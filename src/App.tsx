9c6dfb755ba16956b7b77c4152d6131619effba8
Krasimir Kostadinov <krasimir.kostadinov.dev@gmail.com>
2022-01-06 22:16:10 +0200

    addedd lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home/Home";
-import { Navigation } from "./components/Navigation/Navigation";
-import Footer from "./components/Footer/Footer";
-import RegisterPage from "./pages/RegisterPage/RegisterPage";
-import Account from "./pages/Account/Account";
-import { Box } from "@mui/system";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import RecipesPage from "./pages/RecipesPage/RecipesPage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import React, { Suspense } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home/Home'
+import { Navigation } from './components/Navigation/Navigation'
+
+const Account = React.lazy(() => import('./pages/Account/Account'))
+const RegisterPage = React.lazy(() =>
+  import('./pages/RegisterPage/RegisterPage'),
+)
+const LoginPage = React.lazy(() => import('./pages/LoginPage/LoginPage'))
+const NotFoundPage = React.lazy(() =>
+  import('./pages/NotFoundPage/NotFoundPage'),
+)
+const RecipesPage = React.lazy(() => import('./pages/RecipesPage/RecipesPage'))
+
 // import { useSelector } from "react-redux";
 
 // interface AuthenticationState {
@@ -16,28 +20,25 @@ import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 // }
 
 const App = () => {
-   return (
-      <React.Fragment>
-         <BrowserRouter>
-            <Navigation />
-            <Routes>
-               <Route path="/" element={<Home />} />
-               <Route path="/account" element={<Account />} />
-               <Route path="/register" element={<RegisterPage />} />
-               <Route path="/login" element={<LoginPage />} />
+  return (
+    <React.Fragment>
+      <Suspense fallback={<p>...</p>}>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
 
-               <Route path="/recipes" element={<RecipesPage />}>
-                  <Route
-                     path="/recipes/filtered"
-                     element={<p>Filtered items</p>}
-                  />
-               </Route>
-               <Route path="/recipes/:id" element={<RecipesPage />} />
-               <Route path="*" element={<NotFoundPage />} />
-            </Routes>
-         </BrowserRouter>
-      </React.Fragment>
-   );
-};
+          <Route path="/recipes" element={<RecipesPage />}>
+            <Route path="/recipes/filtered" element={<p>Filtered items</p>} />
+          </Route>
+          <Route path="/recipes/:id" element={<RecipesPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </React.Fragment>
+  )
+}
 
-export default App;
+export default App
