91cc673bdb024027d2ba9670eac46ff541f64db8
Krasimir Kostadinov <krasimir.kostadinov.dev@gmail.com>
2022-01-14 18:20:53 +0200

    fixed images

--- src/components/Recipes/Recipe/Recipe.tsx
+++ src/components/Recipes/Recipe/Recipe.tsx
@@ -1,31 +1,40 @@
 import { Button, Card, Grid, Typography } from "@mui/material";
 import React from "react";
 import { IRecipe } from "../../../repository/recipe-model";
+import mealDefault from "../../../assets/mealDefault.jpg";
 
-interface RecipeProps extends IRecipe {
-}
+interface RecipeProps extends IRecipe {}
 
-export const Recipe: React.FC<RecipeProps> = ({ name, instructions, ingredients, _id, request }) => {
+export const Recipe: React.FC<RecipeProps> = ({
+   name,
+   instructions,
+   ingredients,
+   _id,
+   url,
+}) => {
+   console.log(url);
 
-   console.log(request);
+   let recipeImage = mealDefault;
 
-   let imageSrc;
-   if (request === undefined || request.url || undefined) {
-      imageSrc = "";
-   } else {
-      imageSrc = request.url;
-   }
+   //   useEffect(() => {
+   //    if ()
+
+   //   }, [])
 
    return (
       <Card sx={{ padding: 2 }}>
          <Typography variant="h3">{name}</Typography>
          <Typography variant="body1">{instructions}</Typography>
-         <img alt={name} src={imageSrc} />
+         <img alt={name} src={url || recipeImage} />
          <Grid container component="ul">
             {ingredients.map((ingredient) => (
                <Grid item key={_id}>
-                  <Typography variant="body1">{ingredient.ingredientName}</Typography>
-                  <Typography variant="body2">{ingredient.ingredientPortion}</Typography>
+                  <Typography variant="body1">
+                     {ingredient.ingredientName}
+                  </Typography>
+                  <Typography variant="body2">
+                     {ingredient.ingredientPortion}
+                  </Typography>
                </Grid>
             ))}
             {/* <Grid item>
